17f3bc6d3566a08472a8a1a0a34f1017c357a638
Akshay8281 <aks.pitroda.8@gmail.com>
2025-12-23 08:45:23 +0530

    Fresh build for gh-pages

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-
-// https://vite.dev/config/
-export default defineConfig({
-  base: "/zomato_clone_reactjs/",
-  plugins: [tailwindcss(), react()],
-});
